4196127323e2e67fa0dee59e5e6bf2a7bd9b375b
Renne Rocha <renne@rennerocha.com>
2021-11-02 22:52:24 -0300

    Add balance information to entry list

--- hashi/bookkeeping/tests/views/test_entry_list.py
+++ hashi/bookkeeping/tests/views/test_entry_list.py
@@ -1,3 +1,4 @@
+from bookkeeping.models import EntryType
 from django.test import TestCase
 from django.urls import reverse
 from model_bakery import baker
@@ -26,3 +27,31 @@ class EntryList(TestCase):
         self.assertQuerysetEqual(
             response.context["entries"], [entry_1, entry_2], ordered=False
         )
+
+    def test_entry_list_with_total_income(self):
+        baker.make("bookkeeping.Entry", value=500, type=EntryType.INCOME)
+        baker.make("bookkeeping.Entry", value=400, type=EntryType.INCOME)
+
+        response = self.client.get(reverse("bookkeeping:entry-list"))
+
+        self.assertEqual(response.context["total_income"], 900)
+
+    def test_entry_list_with_total_expense(self):
+        baker.make("bookkeeping.Entry", value=500, type=EntryType.EXPENSE)
+        baker.make("bookkeeping.Entry", value=400, type=EntryType.EXPENSE)
+
+        response = self.client.get(reverse("bookkeeping:entry-list"))
+
+        self.assertEqual(response.context["total_expense"], 900)
+
+    def test_entry_list_with_balance_context(self):
+        baker.make("bookkeeping.Entry", value=100, type=EntryType.EXPENSE)
+        baker.make("bookkeeping.Entry", value=100, type=EntryType.EXPENSE)
+        baker.make("bookkeeping.Entry", value=500, type=EntryType.INCOME)
+        baker.make("bookkeeping.Entry", value=500, type=EntryType.INCOME)
+
+        response = self.client.get(reverse("bookkeeping:entry-list"))
+
+        self.assertEqual(response.context["total_expense"], 200)
+        self.assertEqual(response.context["total_income"], 1000)
+        self.assertEqual(response.context["balance"], 800)
--- hashi/bookkeeping/views.py
+++ hashi/bookkeeping/views.py
@@ -1,7 +1,22 @@
-from bookkeeping.models import Entry
+from bookkeeping.models import Entry, EntryType
+from django.db.models import Sum
 from django.shortcuts import render
 
 
 def entry_list(request):
-    context = {"entries": Entry.objects.all()}
+    entries = Entry.objects.all()
+
+    total_expense, total_income = 0, 0
+    for entry in entries.values("type").annotate(total_value=Sum("value")):
+        if entry["type"] == EntryType.INCOME.value:
+            total_income = entry["total_value"]
+        elif entry["type"] == EntryType.EXPENSE.value:
+            total_expense = entry["total_value"]
+
+    context = {
+        "entries": Entry.objects.all(),
+        "total_income": total_income,
+        "total_expense": total_expense,
+        "balance": total_income - total_expense,
+    }
     return render(request, "bookkeeping/entry_list.html", context=context)
