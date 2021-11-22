97bbdebcdccab9489f1cba37df75f286265e5d79
Renne Rocha <renne@rennerocha.com>
2021-11-22 17:38:07 -0300

    Show entry tags in admin list

--- hashi/bookkeeping/admin.py
+++ hashi/bookkeeping/admin.py
@@ -7,5 +7,12 @@ admin.site.register(Tag)
 
 @admin.register(Entry)
 class EntryAdmin(admin.ModelAdmin):
-    list_display = ("date", "value", "type", "description", "account")
+    list_display = ("date", "value", "type", "description", "account", "entry_tags")
     list_filter = ("type", "account")
+
+    def entry_tags(self, obj):
+        return " | ".join([a.name for a in obj.tags.all().order_by("name")])
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.prefetch_related("tags")
