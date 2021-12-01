7cb5b3d68dde38bf3a1b939eccd31ed96e5a21af
Renne Rocha <renne@rennerocha.com>
2021-12-01 17:17:01 -0300

    Store notification with statuses

--- hashi/webhooks/models.py
+++ hashi/webhooks/models.py
@@ -5,10 +5,16 @@ class GatewayType(models.IntegerChoices):
     PAYPAL = 1, "PAYPAL"
 
 
+class NotificationStatus(models.IntegerChoices):
+    RECEIVED = 1, "RECEIVED"
+
+
 class Notification(models.Model):
     gateway = models.PositiveSmallIntegerField(choices=GatewayType.choices)
     raw_notification = models.TextField()
-    processed = models.BooleanField(default=False)
+    status = models.PositiveSmallIntegerField(
+        choices=NotificationStatus.choices, default=NotificationStatus.RECEIVED
+    )
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- hashi/webhooks/tests/paypal/test_webhook.py
+++ hashi/webhooks/tests/paypal/test_webhook.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-from webhooks.models import GatewayType, Notification
+from webhooks.models import GatewayType, Notification, NotificationStatus
 
 VALID_IPN_NOTIFICATION = "transaction_subject=Transaction Title&payment_date=03:19:29 Nov 30, 2021 PST&txn_type=subscr_payment&subscr_id=I-U5J6K5LBNHJ5&last_name=Raybon&residence_country=BR&item_name=Transaction Title&payment_gross=&mc_currency=BRL&business=contact@receiver.com&payment_type=instant&protection_eligibility=Eligible&verify_sign=AvdDeOMbtfESfLs445gdB-d7.oTyA1cGOw.6du8LpfTZO9OqFvwscLQW&payer_status=verified&payer_email=payer@email.com&txn_id=3FDS5R43543G49105&receiver_email=receiver@email.com&first_name=Abel&payer_id=FWF4354GFGDFG&receiver_id=432F43F4VL7P8&item_number=transaction-title&payment_status=Completed&payment_fee=&mc_fee=5.40&btn_id=423543424&mc_gross=75.00&charset=windows-1252&notify_version=3.9&ipn_track_id=f45345435vc48"
 
@@ -14,7 +14,7 @@ class PaypalWebhook(TestCase):
         )
         self.assertEqual(response.status_code, 200)
 
-    def test_received_ipn_notification_stored_as_not_processed(self):
+    def test_received_ipn_notification_stored_as_received(self):
         self.client.post(
             reverse("webhooks:paypal-listener"),
             content_type="application/x-www-form-urlencoded",
@@ -25,6 +25,6 @@ class PaypalWebhook(TestCase):
             Notification.objects.filter(
                 gateway=GatewayType.PAYPAL,
                 raw_notification=VALID_IPN_NOTIFICATION,
-                processed=False,
+                status=NotificationStatus.RECEIVED,
             ).exists()
         )
