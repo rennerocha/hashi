02edf73f5f02a36bb92e3400550cd9f1d9b155c2
Renne Rocha <renne@rennerocha.com>
2022-03-07 22:50:05 -0300

    Disable CSRF check for PayPal IPN listener endpoint

--- hashi/webhooks/views.py
+++ hashi/webhooks/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from webhooks.models import GatewayType, Notification
 from webhooks.utils import paypal_handshake
 
 
+@csrf_exempt
 def paypal_listener(request):
     raw_notification = request.body.decode("utf-8")
     notification = Notification.objects.create(
